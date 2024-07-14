f0708a453d923abcdc8a3db41be856afa9b54485
PratikAwadhiya <pratikawadhiya02@gmail.com>
2024-07-14 13:25:44 +0530

    02-Day.js "Operators Concept"

---
+++ 02-Day.js
@@ -0,0 +1,165 @@
+// Day-02 14/07/2024
+
+// //Task 1: Write a program to add two number and log the result to the console
+// let num1 = 10;
+// let num2 = 20;
+
+// let sum = num1 + num2;
+// console.log(sum); 
+
+// function myfunction(num1,num2){
+//     let sum = num1 + num2;
+//     console.log(sum);
+// }
+// myfunction(40,20); 
+
+//Task 2: Write a program to subtract two number and log the result to the console
+// let num1 = 20;
+// let num2 = 10;
+
+// let subtract = num1 - num2
+// console.log(subtract);
+
+//Task3: write a program to multiply two number and log the result to the console
+// let num1 = 20;
+// let num2 = 10;
+
+// let multi = num1 * num2
+// console.log(multi);
+
+//Task4: write a program to divide two number and log the result to the console
+// let num1 = 20;
+// let num2 = 10;
+
+// let divide = num1/num2
+// console.log(divide);
+
+//Task4: write a program to find the remainder when one number is divided by another and log the result to the console
+// let num1 = 10;
+// let num2 = 3;
+
+// let remainder = num1%num2
+// console.log(remainder);
+
+// function remainder(divided,divisor){
+//     let remainder = divided % divisor
+//     console.log(remainder)
+// }
+// const num1 = 20;
+// const num2 = 3;
+
+// remainder(num1,num2);
+
+// Assigment Operator
+
+//Task6: Use the += operator to add a number to a variable and log the result to the console
+
+    // let number = 20;
+    // let addition = 4;
+
+    // number += addition;
+    // console.log(number);
+
+//Task7: Use the += operator to Subtract a number to a variable and log the result to the console
+
+    // let number = 20;
+    // let subtract = 4;
+
+    // number -= subtract;
+    // console.log(number);
+
+
+// COMPARISON OPERATORS
+
+//Task 8 : write a program to compare two number using > and < and log the result to the console
+
+// function myfunction(num1,num2){
+//     if(num1 > num2){
+//         console.log(`${num1} is greater than ${num2}`)
+//     }
+//     else if(num1 < num2){
+//         console.log(`${num2} is greater than ${num1}`)
+//     }
+//     else{
+//         console.log(`${num1} is equal to ${num2}`)
+//     }
+// }
+// const num1 = 20;
+// const num2 = 40;
+// myfunction(num1,num2);
+
+//Task 9 : write a program to compare two number using >= and <= and log the result to the console
+
+// let num1 = 15;
+// let num2 = 20;
+
+// if(num1 >= num2)
+// {
+//     console.log(`${num1} is greater than or equal to ${num2}`)
+// }
+// else if(num1 <= num2)
+// {
+//     console.log(`${num2} is greater than or equal to ${num1}`)
+// }
+// else{
+//     console.log(`${num1} is equal to ${num2}`)
+// }
+
+//Task 10 : write a program to compare two number using == and === and log the result to the console
+
+// let num1 = 15;
+// let num2 = 15;
+// if (num1 == num2) {
+//     console.log(`${num1} == ${num2}: They are equal in value.`);
+//   } else {
+//     console.log(`${num1} == ${num2}: They are not equal in value.`);
+//   }
+  
+//   // Using === operator (checks for value and type equality)
+//   if (num1 === num2) {
+//     console.log(`${num1} === ${num2}: They are equal in value and type.`);
+//   } else {
+//     console.log(`${num1} === ${num2}: They are not equal in value or type.`);
+//   }
+
+// LOGICAL OPERATOR
+
+//Task 11 : write a program that uses the && operator to combine two condtion and log the result to the console
+
+// let num1 = 8;
+// let num2 = 12;
+
+// if (num1 > 5 && num2 < 15) {
+//   console.log(`Both conditions are true: ${num1} is greater than 5 and ${num2} is less than 15.`);
+// } else {
+//   console.log(`One or both conditions are false.`);
+// }
+
+// Task12: write a program that uses the || operator to combine two condtion and log the result to the console
+// let num1 = 3;
+// let num2 = 20;
+
+// if (num1 < 5 || num2 > 15) {
+//   console.log(`At least one conditions is true: ${num1} is less than 5 or ${num2} is greater than 15.`);
+// } else {
+//   console.log(`One or both conditions are false.`);
+// }
+
+// Task13: write a program that uses the ! operator to negate a condtion and log the result to the console
+
+// let israining = false;
+
+// if(!israining){
+//     console.log("Go outside and play")
+// }
+// else{
+//     console.log("Stay inside and play")
+// }
+
+// TERNARY OPERATOR
+
+// Task14: write a program that uses the ternary operator to check if a number is positive or negative and log the result
+
+// let num = -5;
+// let result = num > 0 ? "positive" : "negative";  //ternary condition
+// console.log(result);
